59ebef1f8b9034b519dab13c491c3739074e3fea
Abhilekh Dowerah <abhilekh.dowerah@borgos.in>
2022-02-28 16:26:56 +0530

    Route method changed from put to patch

--- routes/task.js
+++ routes/task.js
@@ -5,7 +5,7 @@ const tasksController = require('../controllers/tasks');
 router.route('/createTask').post(tasksController.createTask);
 router.route('/getAllTasks').get(tasksController.getAllTasks);
 router.route('/getTaskById/:id').get(tasksController.getTaskById);
-router.route('/updateTaskById/:id').put(tasksController.updateTaskById);
+router.route('/updateTaskById/:id').patch(tasksController.updateTaskById);
 router.route('/deleteTaskById/:id').delete(tasksController.deleteTaskById);
 
 module.exports = router;
